daf6abc526015728df7002a1249b5830e2d84622
springApp <2663184239@qq.com>
2018-02-24 12:13:21 +0800

    init

---
+++ src/main/java/spring/cloud/app/springconfigdemo/SpringConfigDemoApplication.java
@@ -0,0 +1,12 @@
+package spring.cloud.app.springconfigdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigDemoApplication.class, args);
+	}
+}
